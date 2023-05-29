195e575945856e2b10fdccbf6322a9f99839095c
setuprouter-leipzig <122655051+setuprouter-leipzig@users.noreply.github.com>
2023-05-30 01:15:36 +0200

    Add files via upload
    Test version of the website

---
+++ scripts/createmail.js
@@ -0,0 +1,12 @@
+let a = "rou";
+let b = "ter";
+let c = "stw";
+let d = "kle";
+let e = "ipz";
+let f = "ig@";
+let g = "gma";
+let h = "il.";
+let i = "com";
+console.log(a + b + c + d + e + f + g + h + i);
+document.getElementById("mail").innerText = a + b + c + d + e + f + g + h + i;
+document.getElementById("mail").href = "mai" + "lto" + ":" + a + b + c + d + e + f + g + h + i;
